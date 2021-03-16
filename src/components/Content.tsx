6c9ddb82b156bd96b26f07dc8bf96b59c6799283
Clisciano <clisciano@gmail.com>
2021-03-16 11:53:52 -0300

    structuring the components Content and SideBar

--- src/components/Content.tsx
+++ src/components/Content.tsx
@@ -1,3 +1,44 @@
-export function Content() {
-  // Complete aqui
+import React from 'react';
+
+import { MovieCard } from './MovieCard';
+
+interface Movies {
+  Title: string;
+  Poster: string;
+  Ratings: Array<{
+    Source: string;
+    Value: string;
+  }>;
+  Runtime: string;
+}
+
+interface ContentProps {
+ movies: Array<Movies>;
+ titleGenre: string;
+}
+
+export function Content(props: ContentProps) {
+    
+  return(
+    <>
+        <header>
+          <span className="category">
+            Categoria:
+              <span> {props.titleGenre}</span>
+          </span>
+        </header>
+
+        <main>
+          <div className="movies-list">
+            {props.movies.map(movie => (
+              <MovieCard 
+                title={movie.Title} 
+                poster={movie.Poster} 
+                runtime={movie.Runtime} 
+                rating={movie.Ratings[0].Value} />
+            ))}
+          </div>
+        </main>
+      </>
+  );
 }
